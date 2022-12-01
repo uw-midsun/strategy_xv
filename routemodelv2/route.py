19b5c6cdd6672c076eaf0c22ff190d5a19568cbf
Ryan Lam <53351930+ryan-lam@users.noreply.github.com>
2022-12-01 17:32:07 -0500

    new routebook data + routebook indexing

--- routemodelv2/route.py
+++ routemodelv2/route.py
@@ -31,8 +31,55 @@ polyline_coordinates = [
     (43.46348360838345, -80.53993555488016),
     (43.46266401802443, -80.53908339764159)
     ]
+polyline_coordinates_ASC_BL_2022 = [
+    (40.8829378816515, -98.37406855532743),
+    (40.88303262290332, -98.37402201117517),
+    (40.882921640280436, -98.37347780262579),
+    (40.88294870923001, -98.3729550759928),
+    (40.883056984922696, -98.37254691950501),
+    (40.88323834630254, -98.37222469075867),
+    (40.88354422332468, -98.37191320297053),
+    (40.88393671833501, -98.37168048219625),
+    (40.884356279374096, -98.37165900027982),
+    (40.88480561276309, -98.37186665880527),
+    (40.885187273075935, -98.37230703812195),
+    (40.88531719948494, -98.37257556207723),
+    (40.885379455798876, -98.37281902379671),
+    (40.88545253922308, -98.37316631477889),
+    (40.88646757846244, -98.3731484132071),
+    (40.887044113825155, -98.3730266823367),
+    (40.88793732767246, -98.37301594137848),
+    (40.887888607228646, -98.36852622070076),
+    (40.887542149696934, -98.35897750849163),
+    (40.887275916856474, -98.3397462910507),
+    (40.858084895499736, -98.33990940157038),
+    (40.82569092682914, -98.34058548005243),
+    (40.77376804238631, -98.34005464594843),
+    (40.772412410380845, -98.34062793999414),
+    (40.771898503369655, -98.34121293391789),
+    (40.77143775583309, -98.34204362529023),
+    (40.7710744718689, -98.34309661435378),
+    (40.77098586572269, -98.34404430451097),
+    (40.771010164750734, -98.3782267398604),
+    (40.820256453345706, -98.37835518348754),
+    (40.88797476498994, -98.37820199597991),
+    (40.88788783491318, -98.37317400519893),
+    (40.887254909662055, -98.37317892995024),
+    (40.88673615464531, -98.37324787646557),
+    (40.886482980859384, -98.37323638537788),
+    (40.88545290107626, -98.37333652198858),
+    (40.88540822255482, -98.37384377135761),
+    (40.88527170465601, -98.3742295435631),
+    (40.88498005184619, -98.37464486425907),
+    (40.88471942485049, -98.3748763275634),
+    (40.8844575557321, -98.37498303051221),
+    (40.88412121995178, -98.37503063644006),
+    (40.88373647949922, -98.3749108008285),
+    (40.88368683540823, -98.37514554730026)
+    ]
 interval_upper_bound = 25
-route = RouteClass(polyline_coordinates=polyline_coordinates, interval_upper_bound=interval_upper_bound)
+# route = RouteClass(polyline_coordinates=polyline_coordinates, interval_upper_bound=interval_upper_bound)
+route = RouteClass(polyline_coordinates=polyline_coordinates_ASC_BL_2022, interval_upper_bound=interval_upper_bound)
 
 
 """
@@ -43,7 +90,7 @@ interval_upper_bound = route.interval_upper_bound
 coordinates = get_coordinates(polyline_coordinates=polyline_coordinates, interval_upper_bound=interval_upper_bound)
 
 route.append_data(coordinates)
-# print(route.data())
+print(route.data())
 # route.get_csv("route0")
 # route.to_database(table_name="route0")
 
@@ -67,7 +114,7 @@ IMPORTANT: The start point in the RouteClass route must be the same (or as close
 IMPORTANT: The mapping routebook onto our route data has an accuracy/error upper bound of interval_upper_bound/2 
 """
 trip_distance_list = route.listdata("trip(m)")
-routebook_filepath = "/home/ryanlam/Desktop/strategy_xv/routemodelv2/routebook/SegmentDLoop.csv"
+routebook_filepath = "/home/ryanlam/Desktop/strategy_xv/routemodelv2/routebook/2022_B_Loop.csv"
 print(map_routebook_data(trip_distance_list=trip_distance_list, routebook_filepath=routebook_filepath))
 
 
--- routemodelv2/routebook/map_routebook_data.py
+++ routemodelv2/routebook/map_routebook_data.py
@@ -5,15 +5,30 @@ import pandas as pd
 
 
 
-class TripDistanceError(Exception):
+class RouteMappingError(Exception):
     pass
 
+
 def map_routebook_data(trip_distance_list, routebook_filepath):
+    """
+    map_routebook_data
+    """
+
+
+    """
+    1) Get routebook data and clean/remove empty strings
+    """
     routebook = pd.read_csv(routebook_filepath)
     routebook.replace(r'^\s*$', np.nan, regex=True, inplace = True)
 
-    # Reformat + rename columns and convert values to SI units
-    del routebook['Int']
+
+    """
+    2) Reformat the routebook data
+    - Remove unused columns
+    - Rename columns
+    - Convert data to SI units
+    """
+    del routebook["Int"]
     routebook.rename(inplace=True, columns={
         "Step": "step",
         "Trip": "routebook_trip(m)",
@@ -24,21 +39,50 @@ def map_routebook_data(trip_distance_list, routebook_filepath):
         "Shoulder": "shoulder",
         "Services": "services"
     })
-    routebook['routebook_trip(m)'] = routebook['routebook_trip(m)'].apply(lambda x: x*1609.344) # miles to meters
-    routebook['speed_limit(m/s)'] = routebook['speed_limit(m/s)'].map(float).apply(lambda x: x*0.44704) # mph to m/s
+    routebook["routebook_trip(m)"] = routebook["routebook_trip(m)"].apply(lambda x: x*1609.344) # miles to meters
+    routebook["speed_limit(m/s)"] = routebook["speed_limit(m/s)"].map(float).apply(lambda x: x*0.44704) # mph to m/s
 
-    # if trip_distance_list[-1] < routebook.iloc[-1]['routebook_trip(m)']:
-    #     raise TripDistanceError('The selected route is smaller than the route given in the routebook')
+
+    """
+    3) Index the routebook data to the RouteClass route/data by finding the closest recorded coordinate (from RouteClass)
+    to each datapoint in the routebook using the trip(m) difference
+    """
+    if trip_distance_list[-1] < routebook.iloc[-1]["routebook_trip(m)"]:
+        raise RouteMappingError("The RouteClass route is smaller than the route given in the routebook")
 
     trip_distance_list
-    routebook_trip_distance_list = routebook['routebook_trip(m)'].tolist()
-
-    routebook_data_index = [None] * len(trip_distance_list)
-
+    routebook_trip_distance_list = routebook["routebook_trip(m)"].tolist()
     trip_pointer = 0
     routebook_trip_pointer = 0
+    data_index = [None] * len(trip_distance_list)
 
-    # while 
+    while trip_pointer < len(trip_distance_list)-1 and routebook_trip_pointer < len(routebook_trip_distance_list):
+        routebook_dist = routebook_trip_distance_list[routebook_trip_pointer]
+        dist1 = abs(trip_distance_list[trip_pointer] - routebook_dist)
+        dist2 = abs(trip_distance_list[trip_pointer+1] - routebook_dist)
+
+        if dist1 < dist2:
+            data_index[trip_pointer] = routebook_trip_pointer
+            trip_pointer += 1
+            routebook_trip_pointer += 1
+        else:
+            routebook_trip_pointer += 1
+
+    if routebook_trip_pointer < len(routebook_trip_distance_list):
+        data_index[-1] = routebook_trip_pointer
+        routebook_trip_pointer += 1
+    if routebook_trip_pointer != len(routebook_trip_distance_list):
+        raise RouteMappingError("Unable to map the routebook data to the RouteClass route. Please ensure you have the correct route and is as accurate as possible")
+
+
+    """
+    4) Map the routebook data to the RouteClass route using the previously found routebook-to-RouteClass-route indices
+    """
+    
+
+
+
+
 
 
 

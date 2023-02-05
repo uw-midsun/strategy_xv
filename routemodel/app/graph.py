cad51d74056d05aa61f2f085d18558faa3d15de3
Steven20210 <69274566+Steven20210@users.noreply.github.com>
2023-02-04 22:36:54 -0700

    converted graph to a dash app and added required packages to requirements.txt file

--- routemodel/app/graph.py
+++ routemodel/app/graph.py
@@ -1,27 +1,72 @@
 import pandas as pd
 import asyncio
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import plotly.express as px
+import time
+import atexit
+import plotly
+import dash
+from dash import html, dcc
+from dash.dependencies import Output, Input
+import plotly.graph_objects as go
 
 
 csv_dir = 'sample_route_step4.csv'
+widget = go.FigureWidget()
 
+app = dash.Dash(__name__)
 
-async def display_graph(csv):
-    df = pd.read_csv(csv)
-    fig = px.scatter(df, x="polyline_point_index", y="elevation(m)")
-    fig.show()
+app.layout = html.Div(
+    [
+        dcc.Graph(id = 'live-graph',
+                  animate = True),
+        dcc.Interval(
+            id = 'graph-update',
+            interval = 1000,
+            n_intervals = 0
+        ),
+    ]
+)
 
+@app.callback(
+    Output('live-graph', 'figure'),
+    [ Input('graph-update', 'n_intervals') ]
+)
 
-async def graph_process():
-    # Init message
-    print('\nPress Ctrl-C to quit at anytime!\n')
+def update_graph_scatter(csv):
+    df = pd.read_csv(csv_dir)
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(display_graph, "interval", [csv_dir], seconds=5)
-    scheduler.start()
+    data = plotly.graph_objs.Scatter(
+        x=df["polyline_point_index"],
+        y=df["elevation(m)"],
+        name='Scatter',
+        mode='lines+markers'
+    )
+
+    print("hello")
+
+    return {'data': [data],
+            'layout': go.Layout(xaxis=dict(range=[min(df["polyline_point_index"]),
+                                                  max(df["polyline_point_index"])]), yaxis=dict(range=[min(df["elevation(m)"]), max(df["elevation(m)"])]), )}
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(graph_process())
-    loop.run_forever()
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(update_graph_scatter, "interval", [csv_dir], seconds=5)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
+    app.run_server()
+
+# NOTE: will probably delete later, kept it just in case this is useful
+# async def display_graph(csv):
+#     df = pd.read_csv(csv)
+#     widget.add_scatter(x=df["polyline_point_index"], y=df["elevation(m)"])
+#     # widget.add_scatter(y=[2, 1, 4, 3])
+#
+#
+#
+# async def graph_process():
+#     # Init message
+#     print('\nPress Ctrl-C to quit at anytime!\n')
+#

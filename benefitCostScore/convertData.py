58dd7f1ea2c569bfb052d69fa395ad16171173a5
probro27 <prabhavkhera@hotmail.com>
2023-02-20 20:51:48 -0500

    trying to fix errors

--- benefitCostScore/convertData.py
+++ benefitCostScore/convertData.py
@@ -1,7 +1,28 @@
-from email import header
 import pandas as pd
-from benefit_cost_score import costBenefit
 
+def costBenefit(cumDistance: int, majorTurns: int, stops: int, level1: int, level2: int, level3: int, avgSpeed: int, compTime: int, Status: int):
+    """
+    params:
+      @Cumulative Distance(Miles) (+)
+      @Major turns (+)
+      @Number of stops (+)
+      @Level 1 Turns (Easy) (+)
+      @Level 2 turns (Medium) (++)
+      @Level 3 Turns (Hard) (+++)
+      @Average Speed  | Nothing
+      @Estimated Completion Time
+      @Current Status | Nothing
+    Write a program to calculate the benefit cost score based 
+    on the details mentioned in the benefit cost score page.
+    returns:
+      benefitScore: int
+    """
+    # loss = time + weight avg of turns + no. of stops 
+    # Softmax 
+    turnSigma = level1*1 + level2*2 + level3*3
+    totalTurns = level1 + level2 + level3
+    cul = compTime + (turnSigma/totalTurns)  + stops + majorTurns
+    return (cumDistance/cul)
 
 def numOfTurns(data):
     '''
@@ -40,7 +61,6 @@ def totalTimeAndSpeed(data, totalDistance):
 
     averageSpeed = totalDistance / totalTime
     return totalTime, averageSpeed
-
 
 def cleanSpeedData(data):
     data.at[0, "Int"] = 0
--- benefitCostScore/test/test_convert_data.py
+++ benefitCostScore/test/test_convert_data.py
@@ -1,6 +1,9 @@
 import pytest
 import pandas as pd
-from ..convertData import convertData, numOfTurns, numOfStops, totalTimeAndSpeed, cleanSpeedData
+from ..convertData import convertData, numOfTurns, numOfStops, totalTimeAndSpeed, cleanSpeedData, costBenefit
+
+def test_cost_benefit():
+        assert(costBenefit(11, 8, 9, 5, 4, 3, 2, 1, 0) == 0.5546218487394958)
 
 def test_loops():
     column = ["Loop Name", "Cumulative Distance (Miles)", "Major Turns", "Number of stops", "Level 1 Turns (Easy)", "Level 2 Turns (Medium)", 
@@ -17,28 +20,27 @@ def test_loops():
     pd.testing.assert_frame_equal(convertData(r"data/SegmentELoop.csv"), pd.DataFrame([loopThree], columns=column))
     pd.testing.assert_frame_equal(convertData(r"data/CombinedTest.csv"), pd.DataFrame([combined], columns=column))
 
+def test_stats():
+    loopB = pd.read_csv(r"data/SegmentBLoop.csv")
+    cleanSpeedData(loopB)
 
-loopB = pd.read_csv(r"data/SegmentBLoop.csv")
-cleanSpeedData(loopB)
+    loopD = pd.read_csv(r"data/SegmentDLoop.csv")
+    cleanSpeedData(loopD)
 
-loopD = pd.read_csv(r"data/SegmentDLoop.csv")
-cleanSpeedData(loopD)
+    loopE = pd.read_csv(r"data/SegmentELoop.csv")
+    cleanSpeedData(loopE)
 
-loopE = pd.read_csv(r"data/SegmentELoop.csv")
-cleanSpeedData(loopE)
+    ## number of turns
+    assert(numOfTurns(loopB) == 10)
+    assert(numOfTurns(loopD) == 8)
+    assert(numOfTurns(loopE) == 5)
 
+    ## number of stops
+    assert(numOfStops(loopB) == 3)
+    assert(numOfStops(loopD) == 2)
+    assert(numOfStops(loopE) == 4)
 
-def testNumOfTurns():
-	assert(numOfTurns(loopB) == 10)
-	assert(numOfTurns(loopD) == 8)
-	assert(numOfTurns(loopE) == 5)
-
-def testNumOfStops():
-	assert(numOfStops(loopB) == 3)
-	assert(numOfStops(loopD) == 2)
-	assert(numOfStops(loopE) == 4)
-
-def testTotalTimeAndSpeed():
-	assert(totalTimeAndSpeed(loopB, 21.9) == (0.4785625485625485, 45.76204315565586))
-	assert(totalTimeAndSpeed(loopD, 40.7) == (0.7161643911643915, 56.830527323240716))
-	assert(totalTimeAndSpeed(loopE, 16.4) == (0.3127272727272727, 52.44186046511628))
+    ## total time and speed
+    assert(totalTimeAndSpeed(loopB, 21.9) == (0.4785625485625485, 45.76204315565586))
+    assert(totalTimeAndSpeed(loopD, 40.7) == (0.7161643911643915, 56.830527323240716))
+    assert(totalTimeAndSpeed(loopE, 16.4) == (0.3127272727272727, 52.44186046511628))
